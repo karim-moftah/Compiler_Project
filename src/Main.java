222c4dac1071cb1a0d32250e6e0132e2857f6586
A-bahaa <Ahmed180032@feng.bu.edu.eg>
2023-04-19 18:13:34 +0200

    genIR  function

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,15 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-import java.io.File;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.*;
 import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception{
-
+    public static void main(String[] args) throws Exception {
         String inputFileName = "Inputs/input.java";
         FileInputStream inputFile = new FileInputStream(new File(inputFileName));
         ANTLRInputStream input = new ANTLRInputStream(inputFile);
@@ -16,11 +19,19 @@ public class Main {
 
         JavaParser parser = new JavaParser(token);
         ParseTree tree = parser.compilationUnit();
+        genIR(token,tree);
+
+        // execute java output
+        runIntermediateCode();
+        genHTML(token,tree);
+
+    }
+
+    public static void genIR(CommonTokenStream token, ParseTree tree) throws Exception {
         TokenStreamRewriter rewriter = new TokenStreamRewriter(token);
 
         MyVisitorParser myparser = new MyVisitorParser(rewriter);
         myparser.visit(tree);
         myparser.write(rewriter.getText());
-
     }
 }
--- src/MyVisitorParser.java
+++ src/MyVisitorParser.java
@@ -6,20 +6,19 @@ public class MyVisitorParser extends JavaParserBaseVisitor<String> {
 
     TokenStreamRewriter rewriter;
     int count = 1;
+    int caseCount = 0;
 
     public MyVisitorParser(TokenStreamRewriter rewriter) {
         this.rewriter = rewriter;
     }
 
     void write(String string) throws Exception {
-
         String outputFileName = "outputs/output.java";
         FileOutputStream outputFile = new FileOutputStream(outputFileName);
         BufferedOutputStream buffer = new BufferedOutputStream(outputFile);
         byte[] bytes = string.getBytes();
         buffer.write(bytes);
         buffer.close();
-
     }
 
     @Override
@@ -33,8 +32,9 @@ public class MyVisitorParser extends JavaParserBaseVisitor<String> {
     @Override
     public String visitClassBody(JavaParser.ClassBodyContext ctx) {
 
-        rewriter.insertAfter(ctx.getStart(), "\n\n\tstatic public Integer blocksVisited[] = {};");
-        rewriter.insertAfter(ctx.getStart(), "\n\tstatic public ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(blocksVisited));\n");
+        rewriter.insertAfter(ctx.getStart(), "\n\n\tstatic public String blocksVisited[] = {};");
+        rewriter.insertAfter(ctx.getStart(), "\n\n\t\tstatic public Set<String> blocks = new HashSet<String>();");
+        rewriter.insertAfter(ctx.getStart(), "\n\tstatic public ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(blocksVisited));\n");
 
         return super.visitClassBody(ctx);
     }
@@ -43,7 +43,7 @@ public class MyVisitorParser extends JavaParserBaseVisitor<String> {
     public String visitBlock(JavaParser.BlockContext ctx) {
 
         rewriter.insertAfter(ctx.getStart(), "\t\t// block number " + count + '\n');
-        rewriter.insertAfter(ctx.getStart(), "\t\t\tarrayList.add(" + count + ");");
+        rewriter.insertAfter(ctx.getStart(), "\t\t\tarrayList.add(" + '"' + count+ '"' +");");
         ++count;
 
         return super.visitBlock(ctx);
@@ -53,7 +53,7 @@ public class MyVisitorParser extends JavaParserBaseVisitor<String> {
     public String visitIfbranch(JavaParser.IfbranchContext ctx) {
         if (!ctx.statement().getStart().getText().equals("{")) {
 
-            rewriter.insertBefore(ctx.statement().getStart(), "{" + "\t\t\t\t\t\t\t\t\t\t// block number " + count + '\n' + "\t\t\tarrayList.add(" + count + ");\n\t\t\t");
+            rewriter.insertBefore(ctx.statement().getStart(), "{" + "\t\t\t\t\t\t\t\t\t\t// block number " + count + '\n' + "\t\t\tarrayList.add(" +'"' +count + '"'+");\n\t\t\t");
             rewriter.insertAfter(ctx.statement().getStop(), "\n\t\t\t}");
             ++count;
 
@@ -64,41 +64,16 @@ public class MyVisitorParser extends JavaParserBaseVisitor<String> {
     @Override
     public String visitElseif(JavaParser.ElseifContext ctx) {
         if (!ctx.getChild(1).getText().toString().startsWith("if(")) {
-            // inside else not if else
-            rewriter.insertBefore(ctx.statement().getStart(), "{" + "\t\t\t\t\t\t\t\t\t\t// block number " + count + '\n' + "\t\t\tarrayList.add(" + count + ");\n\t\t\t");
-            rewriter.insertAfter(ctx.statement().getStop(), "\n\t\t\t}");
-            ++count;
+            if(!ctx.statement().getStart().getText().equals("{")){
+                // inside else not if else
+                rewriter.insertBefore(ctx.statement().getStart(), "{" + "\t\t\t\t\t\t\t\t\t\t// block number " + count + '\n' + "\t\t\tarrayList.add(" + '"'+ count + '"'+");\n\t\t\t");
+                rewriter.insertAfter(ctx.statement().getStop(), "\n\t\t\t}");
+                ++count;
+            }
 
         }
         return super.visitElseif(ctx);
     }
 
-    @Override
-    public String visitMethodDeclaration(JavaParser.MethodDeclarationContext ctx) {
-        if (ctx.getChild(1).getText().equals("main")) {
-
-            rewriter.insertBefore(ctx.getStop(),
-                    "\n\t\tblocksVisited = arrayList.toArray(blocksVisited);\n\t\tString visitedBlocks = \"\";\n" +
-                    "\t\tfor(int blockNums: blocksVisited)\n\t\t{\n" +
-                    "\t\t\tif(blockNums != 0)\n"+
-                    "\n\t\t\t\tvisitedBlocks += \"Block #\"+blockNums+\" is visited\\n\";\n" +
-                    "\t\t}\n\twrite(visitedBlocks);\n\t");
-        
-        rewriter.insertAfter(ctx.getStop(),"\n\tpublic static void write(String string)  {\n" +
-                "\t\ttry{\n" +
-                "\n" +
-                "\t\t\tString outputFileName = \"blocksVisited.txt\";\n" +
-                "\t\t\tFileOutputStream outputFile = new FileOutputStream(outputFileName);\n" +
-                "\t\t\tBufferedOutputStream buffer = new BufferedOutputStream(outputFile);\n" +
-                "\t\t\tbyte[] bytes = string.getBytes();\n" +
-                "\t\t\tbuffer.write(bytes);\n" +
-                "\t\t\tbuffer.close();\n" +
-                "\t\t}catch (Exception e) {\n" +
-                "\t\t\te.printStackTrace();\n" +
-                "\t\t}\n" +
-                "\n" +
-                "\t}\n");
-        }
-        return super.visitMethodDeclaration(ctx);
-    }
+    
 }
